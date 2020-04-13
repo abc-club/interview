4ddbcdd047a4be482d37c549c42b3cd61b807e58
zoro <aoping@yougola.com>
2020-04-13 11:49:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env sh
 
-# 当发生错误时中止脚本
+# 确保脚本抛出遇到的错误
 set -e
 
-# 构建
+# 生成静态文件
 npm run build
 
+# 进入生成的文件夹
+cd dist
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
-git push
-git subtree push --prefix dist origin gh-pages
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:abc-club/interview-website.git master:gh-pages
+
+cd -
